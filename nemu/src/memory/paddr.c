059469348ea3339e0c179fe5ddfbca0495bb786d
tracer-ysyx <tracer@ysyx.org>
2024-02-22 23:27:31 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:27:31 up 13:15,  3 users,  load average: 0.80, 1.16, 0.92

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,14 +51,27 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))) {
+#ifdef CONFIG_MTRACE
+    if (CONFIG_MTRACE_START <= addr && addr <= CONFIG_MTRACE_END) {
+      log_write("[MTRACE] Read %d bytes at " FMT_PADDR "\n", len, addr);
+    }
+#endif
+    return pmem_read(addr, len);
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+  if (likely(in_pmem(addr))) {
+    if (CONFIG_MTRACE_START <= addr && addr <= CONFIG_MTRACE_END) {
+      log_write("[MTRACE] Read %d bytes at " FMT_PADDR "\n", len, addr);
+    }
+    pmem_write(addr, len, data);
+    return; 
+  }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
