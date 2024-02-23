d6e2709d3cb9ef7dec230179ed9b52a682f280f5
tracer-ysyx <tracer@ysyx.org>
2024-02-23 22:01:10 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-18-generic #18~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb  7 11:40:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:01:10 up 1 day, 11:49,  6 users,  load average: 1.14, 1.15, 1.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -35,9 +35,52 @@ static uint8_t iringbuf_ptr = 0;
 
 void device_update();
 
+#ifdef CONFIG_FTRACE
+static word_t ftrace_dep = 0;
+extern ElfSymbol elf_symbol_list[];
+extern word_t elf_symbol_list_size;
+
+static word_t get_func_sym_ndx(paddr_t p) {
+  for (word_t i = 0; i < elf_symbol_list_size; ++i) {
+    if (elf_symbol_list[i].addr <= p && p < elf_symbol_list[i].addr + elf_symbol_list[i].size) {
+      return i;
+    }
+  }
+  Assert(0, "PC outside any FUNC symbol area: " FMT_PADDR, p);
+}
+
+static void ftrace(Decode *s) {
+  if (likely(s->dnpc == s->snpc)) return;
+  if (elf_symbol_list_size == 0) return; // no elf file
+  word_t from = get_func_sym_ndx(s->pc), to = get_func_sym_ndx(s->dnpc);
+  if (likely(from == to)) return;
+  log_write("[ftrace] 0x" FMT_PADDR ": ", s->pc);
+  for (int i = 0; i < ftrace_dep; ++i) log_write("| ");
+  if (elf_symbol_list[to].addr == to) { // call, jump to the begging of a func
+    ++ftrace_dep;
+    log_write("call [%s@" FMT_PADDR "] -> [%s@" FMT_PADDR "]\n",
+              elf_symbol_list[from].name,
+              elf_symbol_list[from].addr,
+              elf_symbol_list[to].name,
+              elf_symbol_list[to].addr);
+  } else { // ret, return to calling position
+    --ftrace_dep;
+    log_write("ret [%s@" FMT_PADDR "] -> [%s@" FMT_PADDR "]:" FMT_PADDR "\n",
+              elf_symbol_list[from].name,
+              elf_symbol_list[from].addr,
+              elf_symbol_list[to].name,
+              elf_symbol_list[to].addr,
+              s->dnpc);
+  }
+}
+#endif
+
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+#endif
+#ifdef CONFIG_FTRACE
+  ftrace(_this);
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
