9ef5eef13b39d1789951ca3bc1cf2200238ac940
tracer-ysyx <tracer@ysyx.org>
2024-02-24 23:50:37 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:50:37 up  5:50,  4 users,  load average: 1.71, 2.06, 1.99

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,7 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  for (int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); ++i) {
+  for (int i = 31; i >= 0; --i) {
     if (ref_r->gpr[i] != gpr(i)) return false;
   }
   return true;
