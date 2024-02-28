cce1d8fee80e8c4e6604de22d077b13412a5f1dc
tracer-ysyx <tracer@ysyx.org>
2024-02-28 17:09:10 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:09:10 up 18:01,  3 users,  load average: 1.82, 1.71, 1.75

--- npc/csrc/src/sdb/watchpont.cpp
+++ npc/csrc/src/sdb/watchpont.cpp
@@ -89,7 +89,7 @@ void wps_display() {
 bool check_wps() {
   bool success = false, stop = false;
   for (WP *p = head; p; p = p->next) {
-    word_t val = expr(p->expr, &success);
+    word_t val = 0; // expr(p->expr, &success);
     Assert(success, "Expr become invalid: %s", p->expr);
     if (p->val != val) {
       stop = true;
