021ae26d6f6483aad8020fb9db61087ad4c64b69
tracer-ysyx <tracer@ysyx.org>
2024-02-28 16:46:45 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:46:45 up 17:39,  3 users,  load average: 1.90, 1.72, 1.81

--- npc/csrc/src/driver/driver.cpp
+++ npc/csrc/src/driver/driver.cpp
@@ -25,6 +25,8 @@ void init_top(int argc, char **argv) {
     top->clk = 1; top->eval();
   }
   top->rstn = 1;
+  top->clk = 0; top->eval();
+  top->clk = 1; top->eval();
 }
 
 void init_wave(const char *filename) {
