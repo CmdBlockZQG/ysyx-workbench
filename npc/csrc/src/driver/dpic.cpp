94024799bf77367382d755bff14637159a178591
tracer-ysyx <tracer@ysyx.org>
2024-02-28 14:55:17 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:17 up 15:47,  3 users,  load average: 1.37, 1.48, 2.19

--- npc/csrc/src/driver/dpic.cpp
+++ npc/csrc/src/driver/dpic.cpp
@@ -13,13 +13,13 @@ void halt() {
 }
 
 int mem_read(int raddr) {
-  // Log("Mem read " FMT_ADDR, raddr);
+  Log("Mem read " FMT_ADDR, raddr);
   word_t rdata = addr_read((addr_t)raddr & ~0x3u, 4);
   return *(int *)&rdata;
 }
 
 void mem_write(int waddr, int wdata, char wmask) {
-  // Log("Mem write " FMT_ADDR " " FMT_WORD " %x", waddr, wdata, (uint32_t)wmask);
+  Log("Mem write " FMT_ADDR " " FMT_WORD " %x", waddr, wdata, (uint32_t)wmask);
   waddr = waddr & ~0x3u;
   if (wmask & 0b0001) addr_write(waddr + 0, 1, wdata >> 0);
   if (wmask & 0b0010) addr_write(waddr + 1, 1, wdata >> 8);
