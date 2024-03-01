cf869eb18f8ce480884385acc5453f2b93690a22
tracer-ysyx <tracer@ysyx.org>
2024-03-01 21:44:55 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:44:55 up  5:45,  4 users,  load average: 1.72, 1.86, 1.69

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -26,48 +26,97 @@ static int vtnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   char buf[25];
   buf[0] = '-';
   while (*fmt && t < n - 1) {
-    if (*fmt == '%') {
-      ++fmt;
-      char *src;
-      switch (*fmt) {
-        case '%':
-          src = buf + 1;
-          src[0] = '%';
-          src[1] = '\0';
-          break;
-        case 's':
-          src = va_arg(ap, char *);
-          break;
-        case 'd':
-        case 'x':
-          int x = va_arg(ap, int);
-          int base = *fmt == 'd' ? 10 : 16;
-          char *p = buf + 1;
-          src = x < 0 ? buf : buf + 1; // '-'
-          if (x == 0) {
-            *p++ = '0';
-            *p = '\0';
-          } else {
-            while (x) {
-              int dig = ABS(x % base);
-              *p++ = (dig < 10 ? '0' : 'a' - 10) + dig;
-              x /= base;
-            }
-            *p = '\0';
-            reverse(buf + 1, p - buf - 1);
-          }
-          break;
-        case 'f':
-          
-        default:
-          panic("Not implemented");
-          break;
-      }
-      ++fmt;
-      while (*src && t < n - 1) write_char(&out, *src++), ++t;
-    } else {
+    if (*fmt != '%') {
       write_char(&out, *fmt++);
       ++t;
+      continue;
+    }
+    ++fmt;
+    // ----- flags -----
+    // 0~1+ chars
+    char pad = ' ';
+    int justify = 0; // 0 for right, 1 for left
+    while (1) {
+      switch (*fmt) {
+        case '0':
+          pad = '0';
+          ++fmt;
+          continue;
+      }
+      break;
+    }
+    // ----- field width -----
+    // unsigned int (without leading zero) or * (not implemented yet)
+    int field_width = 0; // 0 for unset
+    if ('1' <= *fmt && *fmt <= '9') {
+      field_width = (*fmt++) + '0';
+      while ('0' <= *fmt && *fmt <= '9') {
+        field_width *= 10;
+        field_width += (*fmt++) + '0';
+      }
+    } else if (*fmt == '*') {
+      field_width = va_arg(ap, int);
+      panic("Not implemented");
+    }
+
+    // ----- precision -----
+
+    // ----- length modifier -----
+
+    // ----- flag override -----
+    if (justify && pad == '0') pad = ' ';
+
+    // ----- conversion specifier -----
+    char *res;
+    switch (*fmt) {
+      case '%':
+        res = buf + 1;
+        res[0] = '%';
+        res[1] = '\0';
+        break;
+      case 's':
+        res = va_arg(ap, char *);
+        break;
+      case 'd':
+      case 'x':
+        int x = va_arg(ap, int);
+        int base = *fmt == 'd' ? 10 : 16;
+        char *p = buf + 1;
+        res = x < 0 ? buf : buf + 1; // '-'
+        if (x == 0) {
+          *p++ = '0';
+          *p = '\0';
+        } else {
+          while (x) {
+            int dig = ABS(x % base);
+            *p++ = (dig < 10 ? '0' : 'a' - 10) + dig;
+            x /= base;
+          }
+          *p = '\0';
+          reverse(buf + 1, p - buf - 1);
+        }
+        break;
+      default:
+        panic("Not implemented");
+        break;
+    }
+    ++fmt;
+    // ----- write result -----
+    int res_len = strlen(res);
+    // justify right
+    if (justify == 0 && field_width > res_len) {
+      int i = field_width - res_len;
+      while (t < n - 1 && i--) {
+        write_char(&out, pad);
+      }
+    }
+    while (*res && t < n - 1) write_char(&out, *res++), ++t;
+    // justify left
+    if (justify == 1 && field_width > res_len) {
+      int i = field_width - res_len;
+      while (t < n - 1 && i--) {
+        write_char(&out, pad);
+      }
     }
   }
   write_char(&out, '\0');
