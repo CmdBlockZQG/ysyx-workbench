e265c15084138e8d472c7d46d6755d967575b5d5
tracer-ysyx <tracer@ysyx.org>
2024-03-01 19:13:37 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:13:37 up  3:14,  4 users,  load average: 1.56, 1.61, 1.58

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,8 +7,66 @@
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
+static void write_char(char **p, char c) {
+  if (*p) *(*p)++ = c;
+  else putch(c);
+}
+
+static void reverse(char *s, size_t n) {
+  char t;
+  for (int i = 0; i < (n >> 1); ++i) {
+    t = s[i];
+    s[i] = s[n - i - 1];
+    s[n - i - 1] = t;
+  }
+}
+
+static int vtnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  size_t t = 0;
+  char int_buf[25];
+  int_buf[0] = '-';
+  while (*fmt && t < n - 1) {
+    if (*fmt == '%') {
+      ++fmt;
+      char *src;
+      if (!strncmp(fmt, "s", 1)) {
+        ++fmt;
+        src = va_arg(ap, char *);
+      } else if (!strncmp(fmt, "d", 1)) {
+        ++fmt;
+        int x = va_arg(ap, int);
+        char *p = int_buf + 1;
+        src = x < 0 ? int_buf : int_buf + 1; // '-'
+        if (x == 0) {
+          *p++ = '0';
+          *p = '\0';
+        } else {
+          while (x) {
+            *p++ = '0' + ABS(x % 10);
+            x /= 10;
+          }
+          *p = '\0';
+          reverse(int_buf + 1, p - int_buf - 1);
+        }
+      } else {
+        panic("Not implemented");
+      }
+      while (*src && t < n - 1) write_char(&out, *src++), ++t;
+    } else {
+      write_char(&out, *fmt++);
+      ++t;
+    }
+  }
+  write_char(&out, '\0');
+  return t;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int t = vtnprintf(NULL, -1, fmt, ap);
+  va_end(ap);
+  return t;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -31,53 +89,8 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   return t;
 }
 
-static void reverse(char *s, size_t n) {
-  char t;
-  for (int i = 0; i < (n >> 1); ++i) {
-    t = s[i];
-    s[i] = s[n - i - 1];
-    s[n - i - 1] = t;
-  }
-}
-
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  size_t t = 0;
-  char buf[25];
-  buf[0] = '-';
-  while (*fmt && t < n) {
-    if (*fmt == '%') {
-      ++fmt;
-      char *src;
-      if (!strncmp(fmt, "s", 1)) {
-        ++fmt;
-        src = va_arg(ap, char *);
-      } else if (!strncmp(fmt, "d", 1)) {
-        ++fmt;
-        int x = va_arg(ap, int);
-        char *p = buf + 1;
-        src = x < 0 ? buf : buf + 1; // '-'
-        if (x == 0) {
-          *p++ = '0';
-          *p = '\0';
-        } else {
-          while (x) {
-            *p++ = '0' + ABS(x % 10);
-            x /= 10;
-          }
-          *p = '\0';
-          reverse(buf + 1, p - buf - 1);
-        }
-      } else {
-        panic("Not implemented");
-      }
-      while (*src && t <= n) *out++ = *src++, ++t;
-    } else {
-      *out++ = *fmt++;
-      ++t;
-    }
-  }
-  if (t < n) *out = '\0';
-  return t;
+  return vtnprintf(out, n, fmt, ap);
 }
 
 #endif
