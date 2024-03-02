9adf5ff7c5e6446f00df6d445e4b941d20468a9b
tracer-ysyx <tracer@ysyx.org>
2024-03-02 17:31:50 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:31:50 up 1 day,  1:32,  4 users,  load average: 1.70, 1.71, 1.63

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -106,7 +106,7 @@ static void audio_io_handler(uint32_t offset, int len, bool is_write) {
       break;
     case reg_samples:
       assert(is_write);
-      samples = audio_base[reg_samples] + 512;
+      samples = audio_base[reg_samples] + 1024;
       break;
     case reg_sbuf_size:
       assert(!is_write);
