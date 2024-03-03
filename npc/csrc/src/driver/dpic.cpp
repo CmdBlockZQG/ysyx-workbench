2a5f908dc5d973196a8e7b09a271e3af093d4459
tracer-ysyx <tracer@ysyx.org>
2024-03-03 17:18:50 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:18:50 up 2 days,  1:19,  6 users,  load average: 3.08, 2.91, 2.83

--- npc/csrc/src/driver/dpic.cpp
+++ npc/csrc/src/driver/dpic.cpp
@@ -28,7 +28,7 @@ int mem_read(int raddr) {
       uint64_t t;
       int s[2];
     } t;
-    t.t = std::chrono::time_point_cast<std::chrono::milliseconds> \
+    t.t = std::chrono::time_point_cast<std::chrono::microseconds> \
           (std::chrono::system_clock::now()).time_since_epoch().count();
     return raddr == rtc_mmio ? t.s[0] : t.s[1];
   }
