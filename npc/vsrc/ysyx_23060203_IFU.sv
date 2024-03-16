f36ed12305e2abb375bc481030246a8961ab9d31
tracer-ysyx <tracer@ysyx.org>
2024-03-16 18:39:28 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:39:28 up 18:16,  3 users,  load average: 1.67, 1.65, 1.55

--- npc/vsrc/ysyx_23060203_IFU.sv
+++ npc/vsrc/ysyx_23060203_IFU.sv
@@ -9,7 +9,7 @@ module ysyx_23060203_IFU (
 
   // 向IDU传递pc和inst
   output reg [31:0] pc,
-  output reg [31:0] inst,
+  output [31:0] inst,
   decouple_if.out inst_out,
 
   // 连接指令内存
@@ -23,9 +23,7 @@ module ysyx_23060203_IFU (
   always @(posedge clk) begin
     rstn_prev <= rstn;
     if (~rstn) begin // 复位
-      inst_out.valid <= 0;
       ram_r.arvalid <= 0;
-      ram_r.rready <= 1;
       pc <= 32'h80000000;
     end else if (rstn & ~rstn_prev) begin // 复位释放
       ram_r.arvalid <= 1;
@@ -33,34 +31,37 @@ module ysyx_23060203_IFU (
     end
   end
 
+  assign inst_out.valid = ram_r.rvalid;
+  assign ram_r.rready = inst_out.ready;
+  assign inst = ram_r.rdata;
+
   always @(posedge clk) begin if (rstn) begin
     // 确认ram收到地址
     if (ram_r.arvalid & ram_r.arready) begin
       ram_r.arvalid <= 0;
     end
 
-    // 从ram接收指令 TEMP: 暂时不考虑错误处理，不管resp
-    if (ram_r.rready & ram_r.rvalid) begin
-      if (~inst_out.valid) begin
-        ram_r.rready <= 1;
-        inst_out.valid <= 1;
-        inst <= ram_r.rdata;
-      end else begin
-        ram_r.rready <= 0;
-        inst_reg <= ram_r.rdata;
-      end
-    end
+    // // 从ram接收指令 TEMP: 暂时不考虑错误处理，不管resp
+    // if (ram_r.rready & ram_r.rvalid) begin
+    //   if (~inst_out.valid) begin
+    //     ram_r.rready <= 1;
+    //     inst_out.valid <= 1;
+    //     inst <= ram_r.rdata;
+    //   end else begin
+    //     ram_r.rready <= 0;
+    //     inst_reg <= ram_r.rdata;
+    //   end
+    // end
 
-    // 向下面的模块传递指令 TEMP: 暂时不考虑错误处理，不管resp
-    if (~ram_r.rready & ~inst_out.valid) begin
-      ram_r.rready <= 1;
-      inst_out.valid <= 1;
-      inst <= inst_reg;
-    end
+    // // 向下面的模块传递指令 TEMP: 暂时不考虑错误处理，不管resp
+    // if (~ram_r.rready & ~inst_out.valid) begin
+    //   ram_r.rready <= 1;
+    //   inst_out.valid <= 1;
+    //   inst <= inst_reg;
+    // end
 
     // 确认下游收到数据
     if (inst_out.valid & inst_out.ready) begin
-      inst_out.valid <= 0;
       // 接收npc
       ram_r.arvalid <= 1;
       ram_r.araddr <= npc;
