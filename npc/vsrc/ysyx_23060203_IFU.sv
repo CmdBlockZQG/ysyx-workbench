baafc891d955f6650fe32c1466cc9b50455406c6
tracer-ysyx <tracer@ysyx.org>
2024-03-16 15:34:52 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:34:52 up 15:11,  3 users,  load average: 0.68, 0.93, 0.98

--- npc/vsrc/ysyx_23060203_IFU.sv
+++ npc/vsrc/ysyx_23060203_IFU.sv
@@ -19,8 +19,7 @@ module ysyx_23060203_IFU (
   `include "DPIC.sv"
 
   reg rstn_prev;
-  reg [31:0] inst_reg, npc_reg;
-  reg [31:0] pc_reg [3];
+  reg [31:0] inst_reg;
 
   always @(posedge clk) begin
     rstn_prev <= rstn;
@@ -33,36 +32,13 @@ module ysyx_23060203_IFU (
     end else if (rstn & ~rstn_prev) begin // 复位释放
       ram_r.arvalid <= 1;
       ram_r.araddr <= pc;
-      pc_reg[0] <= pc;
     end
   end
 
   always @(posedge clk) begin if (rstn) begin
-    // 读入npc
-    if (npc_in.ready & npc_in.valid) begin
-      if (~ram_r.arvalid) begin // 尝试向ram传递地址
-        ram_r.arvalid <= 1;
-        npc_in.ready <= 1;
-        ram_r.araddr <= npc;
-        pc_reg[0] <= npc;
-      end else begin // ram暂时无法接收地址，暂存
-        npc_in.ready <= 0;
-        npc_reg <= npc;
-      end
-    end
-
-    // 向ram传递暂存的地址
-    if (~ram_r.arvalid & ~npc_in.ready) begin
-      ram_r.arvalid <= 1;
-      npc_in.ready <= 1;
-      ram_r.araddr <= npc_reg;
-      pc_reg[0] <= npc_reg;
-    end
-
     // 确认ram收到地址
     if (ram_r.arvalid & ram_r.arready) begin
       ram_r.arvalid <= 0;
-      pc_reg[1] <= pc_reg[0];
     end
 
     // 从ram接收指令 TEMP: 暂时不考虑错误处理，不管resp
@@ -71,11 +47,9 @@ module ysyx_23060203_IFU (
         ram_r.rready <= 1;
         inst_out.valid <= 1;
         inst <= ram_r.rdata;
-        pc <= pc_reg[1];
       end else begin
         ram_r.rready <= 0;
         inst_reg <= ram_r.rdata;
-        pc_reg[2] <= pc_reg[1];
       end
     end
 
@@ -84,7 +58,6 @@ module ysyx_23060203_IFU (
       ram_r.rready <= 1;
       inst_out.valid <= 1;
       inst <= inst_reg;
-      pc <= pc_reg[2];
     end
 
     // 确认下游收到数据
@@ -93,6 +66,10 @@ module ysyx_23060203_IFU (
       if (inst == 32'h100073) begin
         halt();
       end
+      // 接收npc
+      ram_r.arvalid <= 1;
+      ram_r.araddr <= npc;
+      pc <= npc;
     end
   end end
 endmodule
