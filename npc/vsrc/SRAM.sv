373d3966d3c6607d5144e5c5f7503feaab3f3662
tracer-ysyx <tracer@ysyx.org>
2024-03-17 23:14:03 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:14:03 up 1 day, 22:50,  3 users,  load average: 4.16, 4.38, 3.56

--- npc/vsrc/SRAM.sv
+++ npc/vsrc/SRAM.sv
@@ -56,7 +56,7 @@ module SRAM (
       write.awready <= 0;
       if (~write.wready | wdata_handshake) begin
         writing <= 1;
-        writing_max <= lfsr_out + 2;
+        writing_max <= 2; // lfsr_out + 2;
       end
     end
     if (wdata_handshake) begin
@@ -65,7 +65,7 @@ module SRAM (
       write.wready <= 0;
       if (~write.awready | waddr_handshake) begin
         writing <= 1;
-        writing_max <= lfsr_out + 2;
+        writing_max <= 2; // lfsr_out + 2;
       end
     end
 
