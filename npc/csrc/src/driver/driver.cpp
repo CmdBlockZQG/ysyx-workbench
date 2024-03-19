51b46384efb7b0d263b56c2e2f92ae0ae014c7e3
tracer-ysyx <tracer@ysyx.org>
2024-03-20 01:13:47 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:13:47 up 4 days, 50 min,  4 users,  load average: 2.77, 2.26, 2.05

--- npc/csrc/src/driver/driver.cpp
+++ npc/csrc/src/driver/driver.cpp
@@ -21,9 +21,9 @@ void init_wave(const char *filename) {
   if (!filename) return;
   Verilated::traceEverOn(true);
   trace_file = new VerilatedVcdC;
-  top_module->trace(trace_file, 1);
-  printf(">>>>>>>%s<<<<<<<<<<<<<\n", top_module->ysyxSoCFull->name());
-  // trace_file->dumpvars(0, "top");
+  // top_module->trace(trace_file, 1);
+  // printf(">>>>>>>%s<<<<<<<<<<<<<\n", top_module->ysyxSoCFull->name());
+  trace_file->dumpvars(0, "top.ysyxSoCFull");
   trace_file->open(filename);
 
   Log("Wave is dumped to %s", filename);
