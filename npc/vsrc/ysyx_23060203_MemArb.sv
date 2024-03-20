a5b91ba6f51b5cb2b5fff4fb7add11ae7fd8bab3
tracer-ysyx <tracer@ysyx.org>
2024-03-20 22:34:30 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:34:30 up 4 days, 22:11,  3 users,  load average: 1.01, 0.90, 1.05

--- npc/vsrc/ysyx_23060203_MemArb.sv
+++ npc/vsrc/ysyx_23060203_MemArb.sv
@@ -7,30 +7,57 @@ module ysyx_23060203_MemArb (
   axi_if.master ram_r
 );
 
-  reg req_ready;
-  // 约定：dev0表示ifu，dev1表示lsu
-  reg lst_dev;
-  wire res_dev = lst_dev;
-  // FIXME: 如果发生死锁，这里的轮替策略可能要改成LSU优先
-  wire req_dev = (ifu_r.arvalid & lsu_r.arvalid) ? ~lst_dev : lsu_r.arvalid;
+  always @(posedge clk) begin
+    if (~rstn) begin
+      req_ready <= 1;
+      lst_dev <= 0;
+      tmp_flag <= 0;
+    end
+  end
 
   assign ram_r.arsize = 3'b010;
   assign ram_r.arlen = 0;
   assign ram_r.arburst = 0;
   assign ram_r.arid = 0;
 
-  assign ram_r.araddr = req_dev ? lsu_r.araddr : ifu_r.araddr;
-  always_comb begin
-    if (req_ready) begin
-      ram_r.arvalid = req_dev ? lsu_r.arvalid : ifu_r.arvalid;
-      ifu_r.arready = ~req_dev ? ram_r.arready : 0;
-      lsu_r.arready = req_dev ? ram_r.arready : 0;
-    end else begin
-      ram_r.arvalid = 0;
-      ifu_r.arready = 0;
-      lsu_r.arready = 0;
+  // req
+  reg req_ready;
+
+  wire ifu_r_hs = ifu_r.arvalid & ifu_r.arready;
+  wire lsu_r_hs = lsu_r.arvalid & lsu_r.arready;
+
+  wire req_dev = tmp_flag ? 0 : ( // 暂存的请求一定是ifu
+    (ifu_r.arvalid & lsu_r.arvalid) ? 1 : lsu_r.arvalid // 同时优先lsu
+  );
+
+  assign ram_r.arvalid = tmp_flag ? 1 : (
+    req_ready & (req_dev ? lsu_r.arvalid : ifu_r.arvalid)
+  );
+  assign ram_r.araddr = tmp_flag ? tmp_raddr : (
+    req_dev ? lsu_r.araddr : ifu_r.araddr
+  );
+
+  assign ifu_r.arready = req_ready & ram_r.arready;
+  assign lsu_r.arready = req_ready & ram_r.arready;
+
+  reg lst_dev;
+  reg tmp_flag;
+  reg [31:0] tmp_raddr;
+  always @(posedge clk) begin if (rstn) begin
+    if (ram_r.arvalid & ram_r.arready) begin
+      lst_dev <= req_dev;
+      req_ready <= 0;
+      if (ifu_r_hs & lsu_r_hs) begin // 同时读
+        tmp_flag <= 1;
+        tmp_raddr <= ifu_r.araddr;
+        req_ready <= 0;
+      end else if (tmp_flag) begin // 暂存读
+        tmp_flag <= 0;
+      end
     end
-  end
+  end end
+  // res
+  wire res_dev = lst_dev;
 
   assign ifu_r.rdata = ram_r.rdata[31:0];
   assign lsu_r.rdata = ram_r.rdata[31:0];
@@ -40,22 +67,9 @@ module ysyx_23060203_MemArb (
   assign lsu_r.rvalid = res_dev ? ram_r.rvalid : 0;
   assign ram_r.rready = res_dev ? lsu_r.rready : ifu_r.rready;
 
-  always @(posedge clk) begin
-    if (~rstn) begin
-      req_ready <= 1;
-      lst_dev <= 0;
-    end
-  end
-
   always @(posedge clk) begin if (rstn) begin
-    if (ram_r.arvalid & ram_r.arready) begin
-      lst_dev <= req_dev;
-      // res_dev <= req_dev;
-      req_ready <= 0;
-    end
-
     if (ram_r.rvalid & ram_r.rready) begin
-      req_ready <= 1;
+      req_ready <= ~tmp_flag;
     end
   end end
 endmodule
