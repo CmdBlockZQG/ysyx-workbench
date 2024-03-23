9f523d76326a030b928c06a3b14e66056a2d5457
tracer-ysyx <tracer@ysyx.org>
2024-03-23 23:05:37 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:05:37 up 7 days, 22:42,  7 users,  load average: 1.66, 1.90, 1.97

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -10,7 +10,7 @@ AM_SRCS := riscv/ysyxsoc/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/am/src/riscv/ysyxsoc/linker.ld
-LDFLAGS   += --gc-sections -e _start
+LDFLAGS   += --gc-sections -e _start # --orphan-handling=warn # --print-map
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/riscv/ysyxsoc/include
 .PHONY: $(AM_HOME)/am/src/riscv/ysyxsoc/trm.c run
