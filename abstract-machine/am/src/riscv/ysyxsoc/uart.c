cc43d06240996e87112fa3b4565104e7eabb7506
tracer-ysyx <tracer@ysyx.org>
2024-03-24 15:06:39 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:39 up 8 days, 14:43,  7 users,  load average: 1.40, 1.55, 1.89

--- abstract-machine/am/src/riscv/ysyxsoc/uart.c
+++ abstract-machine/am/src/riscv/ysyxsoc/uart.c
@@ -3,12 +3,14 @@
 
 #define LCR_ADDR UART_ADDR + 3
 #define DIV_ADDR UART_ADDR
+#define FCR_ADDR UART_ADDR + 2
+#define IER_ADDR UART_ADDR + 1
 
 void __am_uart_init() {
   outb(LCR_ADDR, 0b10000011);
-
   outb(DIV_ADDR + 1, 0);
   outb(DIV_ADDR, 10);
-
   outb(LCR_ADDR, 0b00000011);
+  outb(FCR_ADDR, 0b11000110);
+  outb(IER_ADDR, 0b00000000);
 }
