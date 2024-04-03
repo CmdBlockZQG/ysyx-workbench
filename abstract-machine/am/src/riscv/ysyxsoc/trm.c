13cd3f7ee295edaf355ed1b7d02f4ecd069da851
tracer-ysyx <tracer@ysyx.org>
2024-04-04 01:32:55 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:32:55 up 1 day, 16:38,  5 users,  load average: 1.21, 1.16, 1.40

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -30,8 +30,8 @@ void _trm_init() {
   while (dst < &_data_end) *dst++ = *src++;
   for (dst = &_bss_start; dst < &_bss_end; ++dst) *dst = 0;
 
-  __am_uart_init();
 
   int ret = main(mainargs);
+  __am_uart_init();
   halt(ret);
 }
