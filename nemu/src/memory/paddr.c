f679f12196fef9b4a0e84849a2bacfa62bbdfc6d
tracer-ysyx <tracer@ysyx.org>
2024-04-15 15:48:39 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:48:39 up 22:35,  5 users,  load average: 1.41, 1.67, 1.92

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -28,10 +28,12 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 static uint8_t mrom [MROM_SIZE] PG_ALIGN;
 static uint8_t sram [SRAM_SIZE] PG_ALIGN;
 static uint8_t flash [FLASH_SIZE] PG_ALIGN;
+static uint8_t psram [PSRAM_SIZE] PG_ALIGN;
 const MemMap mem_map[] = {
   { "mrom", MROM_BASE, MROM_SIZE, mrom, true },
   { "sram", SRAM_BASE, SRAM_SIZE, sram, false },
-  { "flash", FLASH_BASE, FLASH_SIZE, flash, true }
+  { "flash", FLASH_BASE, FLASH_SIZE, flash, true },
+    { "psram", PSRAM_BASE, PSRAM_SIZE, psram, false}
 };
 
 static const MemMap *get_mem_map(paddr_t addr) {
