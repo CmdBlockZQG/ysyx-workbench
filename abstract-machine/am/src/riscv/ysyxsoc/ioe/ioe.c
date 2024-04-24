56cb81aaaaa2fd25e3a49045c62d9c6c7f754b94
tracer-ysyx <tracer@ysyx.org>
2024-04-24 15:12:15 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:12:15 up  3:17,  4 users,  load average: 1.54, 1.45, 1.32

--- abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe/ioe.c
@@ -6,10 +6,14 @@ void __am_timer_init();
 void __am_timer_rtc(AM_TIMER_RTC_T *);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *);
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
+void __am_gpio_out(AM_GPIO_OUT_T *);
+void __am_gpio_in(AM_GPIO_IN_T *);
+void __am_gpio_seg(AM_GPIO_SEG_T *);
 
-static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
+static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = false; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
-static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
+static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
+static void __am_gpio_config(AM_GPIO_CONFIG_T *cfg) { cfg->present = true; }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
@@ -18,7 +22,11 @@ static void *lut[128] = {
   [AM_TIMER_UPTIME] = __am_timer_uptime,
   [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
-  [AM_UART_CONFIG]  = __am_uart_config,
+  [AM_UART_CONFIG ] = __am_uart_config,
+  [AM_GPIO_CONFIG ] = __am_gpio_config,
+  [AM_GPIO_OUT    ] = __am_gpio_out,
+  [AM_GPIO_IN     ] = __am_gpio_in,
+  [AM_GPIO_SEG    ] = __am_gpio_seg,
 };
 
 static void fail(void *buf) { panic("access nonexist register"); }
--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -1,12 +1,13 @@
 AM_SRCS := riscv/ysyxsoc/start.S \
            riscv/ysyxsoc/bootloader.c \
            riscv/ysyxsoc/trm.c \
-           riscv/ysyxsoc/ioe.c \
-           riscv/ysyxsoc/timer.c \
            riscv/ysyxsoc/input.c \
-           riscv/ysyxsoc/uart.c \
            riscv/ysyxsoc/cte.c \
            riscv/ysyxsoc/trap.S \
+           riscv/ysyxsoc/ioe/ioe.c \
+           riscv/ysyxsoc/ioe/uart.c \
+           riscv/ysyxsoc/ioe/timer.c \
+           riscv/ysyxsoc/ioe/gpio.c \
            platform/dummy/vme.c \
            platform/dummy/mpe.c
 
