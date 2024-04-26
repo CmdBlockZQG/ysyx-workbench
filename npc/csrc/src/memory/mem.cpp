990f574de25606eb8968e454a1dc91dc820dea5c
tracer-ysyx <tracer@ysyx.org>
2024-04-26 19:43:10 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:43:10 up 1 day,  3:00,  9 users,  load average: 1.30, 1.23, 1.81

--- npc/csrc/src/memory/mem.cpp
+++ npc/csrc/src/memory/mem.cpp
@@ -4,16 +4,16 @@
 #include <cstring>
 
 static uint8_t mrom [MROM_SIZE] PG_ALIGN;
-static uint8_t sram [SRAM_SIZE] PG_ALIGN;
+// static uint8_t sram [SRAM_SIZE] PG_ALIGN;
 static uint8_t flash [FLASH_SIZE] PG_ALIGN;
-static uint8_t psram [PSRAM_SIZE] PG_ALIGN;
-static uint8_t sdram [SDRAM_SIZE] PG_ALIGN;
+// static uint8_t psram [PSRAM_SIZE] PG_ALIGN;
+// static uint8_t sdram [SDRAM_SIZE] PG_ALIGN;
 const MemMap mem_map[] = {
   { "mrom", MROM_BASE, MROM_SIZE, mrom, true },
-  { "sram", SRAM_BASE, SRAM_SIZE, sram, false },
+  { "sram", SRAM_BASE, SRAM_SIZE, nullptr, false },
   { "flash", FLASH_BASE, FLASH_SIZE, flash, true },
-  { "psram", PSRAM_BASE, PSRAM_SIZE, psram, false },
-  { "sdram", SDRAM_BASE, SDRAM_SIZE, sdram, false }
+  { "psram", PSRAM_BASE, PSRAM_SIZE, nullptr, false },
+  { "sdram", SDRAM_BASE, SDRAM_SIZE, nullptr, false }
 };
 
 const MemMap *get_mem_map(addr_t addr, bool panic_if_out) {
@@ -36,10 +36,7 @@ uint8_t *guest_to_host(addr_t addr) {
 
 void init_mem() {
   memset(mrom, 0xCB, MROM_SIZE);
-  memset(sram, 0xCB, SRAM_SIZE);
-  for (uint8_t *p = flash; p < flash + FLASH_SIZE; ++p) {
-    *p = (uint8_t)(uintptr_t)p;
-  }
+  memset(flash, 0xCB, SRAM_SIZE);
 }
 
 word_t addr_read(addr_t addr, int len) {
