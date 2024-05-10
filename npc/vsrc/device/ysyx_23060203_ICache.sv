25353ea725a31714ef6e2312f3744c1f7a4fa095
tracer-ysyx <tracer@ysyx.org>
2024-05-10 22:57:57 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.5.0-28-generic #29~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Apr  4 14:39:20 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:57:57 up 4 days,  7:50,  7 users,  load average: 1.58, 1.43, 1.71

---
+++ npc/vsrc/device/ysyx_23060203_ICache.sv
@@ -0,0 +1,22 @@
+module ysyx_23060203_ICache (
+  input rstn, clk,
+
+  axi_if.master ifu_in,
+  axi_if.master ram_out
+);
+
+  parameter OFFSET_W = 2; // 块字节数=2^
+  parameter INDEX_W  = 4; // 块数=2^
+  parameter TAG_W    = 32 - OFFSET_W - INDEX_W;
+
+  assign ram_out.arsize = 3'b010;
+
+  assign ifu_in.arready = ram_out.arready;
+  assign ram_out.arvalid = ifu_in.arvalid;
+  assign ram_out.araddr = ifu_in.araddr;
+
+  assign ram_out.rready = ifu_in.rready;
+  assign ifu_in.rvalid = ram_out.rvalid;
+  assign ifu_in.rdata = ram_out.rdata;
+
+endmodule
