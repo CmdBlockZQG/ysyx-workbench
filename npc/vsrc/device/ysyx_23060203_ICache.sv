9182adb39eb44f9822af4629348d56e08a03ebcc
tracer-ysyx <tracer@ysyx.org>
2024-07-03 11:34:08 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:34:08 up  2:42,  1 user,  load average: 0.39, 0.44, 0.41

--- npc/vsrc/device/ysyx_23060203_ICache.sv
+++ npc/vsrc/device/ysyx_23060203_ICache.sv
@@ -1,52 +1,130 @@
+// module ysyx_23060203_ICache_new (
+//   input rstn, clk,
+
+//   axi_if.slave ifu_in,
+//   axi_if.master ram_out
+// );
+//   parameter OFFSET_W = 4; // 块内地址宽度，块大小=2^x字节
+//   parameter INDEX_W  = 2; // 组地址宽度，组数=2^x
+//   parameter TAG_W    = 32 - OFFSET_W - INDEX_W; // 标记字宽度
+
+//   parameter SET_N = 1 << INDEX_W; // 组数
+//   parameter BLOCK_W = (1 << OFFSET_W) << 3; // 块位宽
+
+//   // TEMP: 直接映射实现
+//   reg line_valid [SET_N];
+//   reg [TAG_W-1:0] line_tag [SET_N];
+//   reg [BLOCK_W-1:0] line_data [SET_N];
+
+//   wire ifu_in_ar_hs = ifu_in.arvalid & ifu_in.arready;
+//   reg [31:0] addr_reg;
+//   wire [31:0] addr = ifu_in_ar_hs ? ifu_in.araddr : addr_reg;
+//   wire [31:0] aligned_addr = {addr[31:OFFSET_W], {OFFSET_W{1'b0}}};
+
+//   wire enable = addr[31:28] >= 4'h3; // 覆盖flash和sdram，排除很快的sram
+//   wire [INDEX_W-1:0] index = addr[OFFSET_W+INDEX_W-1:OFFSET_W];
+//   wire [TAG_W-1:0] tag = addr[31:OFFSET_W+INDEX_W];
+
+//   wire cache_hit = line_valid[index] & (line_tag[index] == tag);
+//   wire [BLOCK_W-1:0] cache_out = line_data[index];
+
+//   reg cache_resp_valid;
+
+//   assign ram_out.arsize = 3'b010; // 4 Bytes
+
+//   assign ram_out.arvalid = ~cache_hit & ifu_in.arvalid;
+//   assign ram_out.araddr = addr;
+
+//   assign ram_out.rready = 1;
+//   assign ifu_in.rvalid = cache_hit ? cache_resp_valid : ram_out.rvalid;
+//   assign ifu_in.rdata = cache_hit ? {2{cache_out}} : ram_out.rdata; // TEMP: 块大小32
+
+//   integer i;
+//   always @(posedge clk) if (~rstn) begin
+//     for (i = 0; i < SET_N; i = i + 1) line_valid[i] <= 0;
+//     ifu_in.arready <= 1;
+//     cache_resp_valid <= 0;
+//   end else begin
+//     if (ifu_in_ar_hs) begin
+//       addr_reg <= ifu_in.araddr;
+//       if (cache_hit) cache_resp_valid <= 1;
+//       else ifu_in.arready <= 0;
+//     end
+
+//     if (ifu_in.rvalid & ifu_in.rready) begin
+//       if (cache_resp_valid) begin
+//         cache_resp_valid <= 0;
+// `ifndef SYNTHESIS
+//         perf_event(PERF_ICACHE_HIT);
+// `endif
+//       end else begin
+// `ifndef SYNTHESIS
+//         perf_event(PERF_ICACHE_MISS);
+// `endif
+//       end
+//     end
+
+//     if (ram_out.rvalid & ram_out.rready) begin
+//       ifu_in.arready <= 1;
+//       if (enable) begin
+//         line_valid[index] <= 1;
+//         line_tag[index] <= tag;
+//         line_data[index] <= addr[2] ? ram_out.rdata[63:32] : ram_out.rdata[31:0];
+//       end
+//     end
+//   end
+
+// endmodule
+
 module ysyx_23060203_ICache (
   input rstn, clk,
 
   axi_if.slave ifu_in,
   axi_if.master ram_out
 );
+  parameter OFFSET_W = 2; // 块内地址宽度，块大小=2^x字节
+  parameter INDEX_W  = 4; // 组地址宽度，组数=2^x
+  parameter TAG_W    = 32 - OFFSET_W - INDEX_W; // 标记字宽度
 
-  parameter OFFSET_W = 2; // 块字节数=2^x
-  parameter INDEX_W  = 4; // 块数=2^x
-  parameter TAG_W    = 32 - OFFSET_W - INDEX_W;
-
-  parameter ROW_N = 1 << INDEX_W; // 块数
+  parameter SET_N = 1 << INDEX_W; // 组数
   parameter BLOCK_W = (1 << OFFSET_W) << 3; // 块位宽
+
+  // TEMP: 直接映射实现
+  reg line_valid [SET_N];
+  reg [TAG_W-1:0] line_tag [SET_N];
+  reg [BLOCK_W-1:0] line_data [SET_N];
 
   wire ifu_in_ar_hs = ifu_in.arvalid & ifu_in.arready;
   reg [31:0] addr_reg;
   wire [31:0] addr = ifu_in_ar_hs ? ifu_in.araddr : addr_reg;
 
-  wire enable = addr[31:28] >= 4'h3; // 覆盖flash和sdram
+  wire enable = addr[31:28] >= 4'h3; // 覆盖flash和sdram，排除很快的sram
   wire [INDEX_W-1:0] index = addr[OFFSET_W+INDEX_W-1:OFFSET_W];
   wire [TAG_W-1:0] tag = addr[31:OFFSET_W+INDEX_W];
 
-  reg row_valid [ROW_N];
-  reg [TAG_W-1:0] row_tag [ROW_N];
-  reg [BLOCK_W-1:0] row_data [ROW_N];
-
-  wire cache_valid = row_valid[index] & (row_tag[index] == tag);
-  wire [BLOCK_W-1:0] cache_out = row_data[index];
+  wire cache_hit = line_valid[index] & (line_tag[index] == tag);
+  wire [BLOCK_W-1:0] cache_out = line_data[index];
 
   reg cache_resp_valid;
 
-  assign ram_out.arsize = 3'b010;
+  assign ram_out.arsize = 3'b010; // 4 Bytes
 
-  assign ram_out.arvalid = ~cache_valid & ifu_in.arvalid;
+  assign ram_out.arvalid = ~cache_hit & ifu_in.arvalid;
   assign ram_out.araddr = addr;
 
   assign ram_out.rready = 1;
-  assign ifu_in.rvalid = cache_valid ? cache_resp_valid : ram_out.rvalid;
-  assign ifu_in.rdata = cache_valid ? {2{cache_out}} : ram_out.rdata; // TEMP: 块大小32
+  assign ifu_in.rvalid = cache_hit ? cache_resp_valid : ram_out.rvalid;
+  assign ifu_in.rdata = cache_hit ? {2{cache_out}} : ram_out.rdata; // TEMP: 块大小32
 
   integer i;
   always @(posedge clk) if (~rstn) begin
-    for (i = 0; i < ROW_N; i = i + 1) row_valid[i] <= 0;
+    for (i = 0; i < SET_N; i = i + 1) line_valid[i] <= 0;
     ifu_in.arready <= 1;
     cache_resp_valid <= 0;
   end else begin
     if (ifu_in_ar_hs) begin
       addr_reg <= ifu_in.araddr;
-      if (cache_valid) cache_resp_valid <= 1;
+      if (cache_hit) cache_resp_valid <= 1;
       else ifu_in.arready <= 0;
     end
 
@@ -66,9 +144,9 @@ module ysyx_23060203_ICache (
     if (ram_out.rvalid & ram_out.rready) begin
       ifu_in.arready <= 1;
       if (enable) begin
-        row_valid[index] <= 1;
-        row_tag[index] <= tag;
-        row_data[index] <= addr[2] ? ram_out.rdata[63:32] : ram_out.rdata[31:0];
+        line_valid[index] <= 1;
+        line_tag[index] <= tag;
+        line_data[index] <= addr[2] ? ram_out.rdata[63:32] : ram_out.rdata[31:0];
       end
     end
   end
