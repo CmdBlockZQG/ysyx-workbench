b4200b8eea8c3bbee5eefce21e6efe8648af752e
tracer-ysyx <tracer@ysyx.org>
2024-07-03 16:35:52 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:35:52 up  7:44,  1 user,  load average: 0.46, 0.75, 0.67

--- abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
+++ abstract-machine/am/src/riscv/ysyxsoc/bootloader.c
@@ -10,13 +10,13 @@ void _fsbl() {
 }
 
 void _ssbl() {
-  extern char _text_src, _text_start, _text_end;
-  char *src = &_text_src, *dst = &_text_start;
-  while (dst < &_text_end) *dst++ = *src++;
-
   extern char _rodata_src, _rodata_start, _rodata_end;
-  src = &_rodata_src; dst = &_rodata_start;
+  char *src = &_rodata_src, *dst = &_rodata_start;
   while (dst < &_rodata_end) *dst++ = *src++;
+
+  extern char _text_src, _text_start, _text_end;
+  src = &_text_src, dst = &_text_start;
+  while (dst < &_text_end) *dst++ = *src++;
 
   asm volatile("tail _start");
 }
