6c355eeb15f46860647f57b63b526fac14b93943
tracer-ysyx <tracer@ysyx.org>
2024-07-04 19:38:51 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:38:51 up 10:44,  1 user,  load average: 0.41, 0.21, 0.19

--- npc/vsrc/ysyx_23060203_IFU.sv
+++ npc/vsrc/ysyx_23060203_IFU.sv
@@ -29,12 +29,13 @@ module ysyx_23060203_IFU (
   // TEMP: 暂时不考虑错误处理
   assign inst_out.valid = ram_r.rvalid;
   assign ram_r.rready = inst_out.ready;
-  assign ram_r.arvalid = inst_out.ready & rstn;
+  // assign ram_r.arvalid = inst_out.ready & rstn;
   assign inst = ram_r.araddr[2] ? ram_r.rdata[63:32] : ram_r.rdata[31:0];
 
   always @(posedge clk) begin if (rstn) begin
     // 确认ram收到地址
     if (ram_r.arvalid & ram_r.arready) begin
+      ram_r.arvalid <= 0;
       pc <= ram_r.araddr;
     end
 
@@ -42,6 +43,7 @@ module ysyx_23060203_IFU (
     if (inst_out.valid & inst_out.ready) begin
       // 接收npc
       ram_r.araddr <= npc;
+      ram_r.arvalid <= 1;
 `ifndef SYNTHESIS
       if (inst == 32'h100073) begin
         halt();
