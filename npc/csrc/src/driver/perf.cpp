122a0060b92a3b42b51e2c0ebf8086017cffa7fa
tracer-ysyx <tracer@ysyx.org>
2024-07-13 11:14:09 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:14:09 up 2 days,  2:03,  1 user,  load average: 1.08, 0.68, 0.55

--- npc/csrc/src/driver/perf.cpp
+++ npc/csrc/src/driver/perf.cpp
@@ -11,27 +11,30 @@
 // CPLT  : 完成 complete 的缩写
 
 static const char *perfcnt_name[] = {
-  "IFU_WAIT",
-  "IFU_INST",
-  "IFU_HOLD",
+  "IFU_WAIT", // IFU等待指令读取周期数
+  "IFU_INST", // IFU提供指令次数
+  "IFU_HOLD", // IFU提供指令周期数
 
-  "ICACHE_HIT",
-  "ICACHE_MISS",
-  "ICACHE_MEM",
+  "ICACHE_HIT",  // ICache命中次数
+  "ICACHE_MISS", // ICache缺失次数
+  "ICACHE_MEM",  // ICache等待存储器周期数
 
-  "IDU_IDLE",
-  "IDU_INST",
-  "IDU_HOLD",
+  "IDU_IDLE", // IDU空闲周期数
+  "IDU_INST", // IDU提供指令次数
+  "IDU_HOLD", // IDU提供指令周期数
 
-  "EXU_IDLE",
-  "EXU_INST",
-  "EXU_HOLD",
-  "EXU_FLUSH",
-  "EXU_MEMR",
-  "EXU_MEMW",
+  "IDU_LOAD",    // IDU译码LOAD指令
+  "IDU_STORE",  // IDU译码STORE指令
+  "IDU_BRANCH", // IDU译码BRANCH指令
+  "IDU_JAL",    // IDU译码JAL指令
+  "IDU_JALR",   // IDU译码JALR指令
 
-  "WBU_IDLE",
-  "WBU_INST"
+
+  "EXU_IDLE",  // EXU空闲周期数
+  "EXU_INST",  // EXU执行指令数量
+  "EXU_FLUSH", // EXU冲刷上游流水级次数
+  "EXU_MEMR",  // EXU读内存周期数
+  "EXU_MEMW",  // EXU写内存周期数
 };
 
 const int perfcnt_num = ARRLEN(perfcnt_name);
