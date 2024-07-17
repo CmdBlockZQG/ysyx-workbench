801f6cd01ad8e33396cf7d7ba6e50282f5682c99
tracer-ysyx <tracer@ysyx.org>
2024-07-17 12:30:23 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:30:23 up 6 days,  3:20,  1 user,  load average: 1.18, 0.99, 0.83

--- npc/vsrc/exu/ysyx_23060203_EXU.sv
+++ npc/vsrc/exu/ysyx_23060203_EXU.sv
@@ -28,7 +28,6 @@ module ysyx_23060203_EXU (
   input        in_alu_src,
   input [2:0]  in_alu_funct,
   input        in_alu_sw,
-  input        in_mul,
   input [4:0]  in_rd,
   input        in_rd_src,
   input [3:0]  in_ls,
@@ -60,7 +59,6 @@ module ysyx_23060203_EXU (
   reg        alu_src;
   reg [2:0]  alu_funct;
   reg        alu_sw;
-  reg        mul;
   reg [4:0]  rd;
   reg        rd_src;
   reg [3:0]  ls;
@@ -89,7 +87,6 @@ module ysyx_23060203_EXU (
       alu_src <= in_alu_src;
       alu_funct <= in_alu_funct;
       alu_sw <= in_alu_sw;
-      mul <= in_mul;
       rd <= in_rd;
       rd_src <= in_rd_src;
       ls <= in_ls;
@@ -104,18 +101,14 @@ module ysyx_23060203_EXU (
     end
   end
 
-  // 对于不需要功能单元的指令，EXU只需要一周期，而且WBU从不阻塞
-  assign in_ready = lsu_in_ready & mul_in_ready & div_in_ready;
-  assign out_valid = valid & exec_out_valid;
+  assign in_ready = lsu_in_ready;
+  assign out_valid = valid & exe_done;
 
-  reg exec_out_valid;
+  reg exe_done;
   always_comb begin
-    exec_out_valid = 1;
-    if (ls[3]) exec_out_valid = lsu_out_valid;
-    else if (mul) exec_out_valid = alu_funct[2] ? div_out_valid : mul_out_valid;
+    exe_done = 1;
+    if (ls[3]) exe_done = lsu_out_valid;
   end
-
-  wire exec_in_en = in_valid & in_ready;
 
   // -------------------- ALU --------------------
   wire [31:0] alu_a = alu_src ? pc : val_a;
@@ -128,44 +121,16 @@ module ysyx_23060203_EXU (
   );
 
   // -------------------- LSU --------------------
-  wire lsu_in_en = |in_ls;
+  wire lsu_in_valid = in_valid & (|in_ls);
   wire lsu_in_ready, lsu_out_valid;
   wire [31:0] lsu_out_rdata;
   ysyx_23060203_LSU LSU (
     .clock(clock), .reset(reset),
     .mem_r(mem_r), .mem_w(mem_w),
-    .in_ready(lsu_in_ready), .in_valid(exec_in_en & lsu_in_en),
+    .in_ready(lsu_in_ready), .in_valid(lsu_in_valid),
     .in_ls(in_ls), .ls(ls), .alu_val(alu_val), .val_c(val_c),
     .out_ready(out_ready), .out_valid(lsu_out_valid),
     .out_rdata(lsu_out_rdata)
-  );
-
-  // -------------------- MUL --------------------
-  wire mul_in_en = in_mul & ~in_alu_funct[2];
-  wire [1:0] mul_in_sign = {^in_alu_funct[1:0], ~in_alu_funct[1] & in_alu_funct[0]};
-  wire mul_in_ready, mul_out_valid;
-  wire [63:0] mul_out_prod;
-  wire [31:0] mul_val = (|alu_funct[1:0]) ? mul_out_prod[63:32] : mul_out_prod[31:0];
-  ysyx_23060203_MUL_radix_4 MUL (
-    .clock(clock), .reset(reset), .flush(0),
-    .in_ready(mul_in_ready), .in_valid(exec_in_en & mul_in_en),
-    .in_sign(mul_in_sign), .in_a(in_val_a), .in_b(in_val_b),
-    .out_ready(out_ready), .out_valid(mul_out_valid),
-    .out_prod(mul_out_prod)
-  );
-
-  // -------------------- DIV --------------------
-  wire div_in_en = in_mul & in_alu_funct[2];
-  wire div_in_sign = ~in_alu_funct[0];
-  wire div_in_ready, div_out_valid;
-  wire [31:0] div_out_quot, div_out_rem;
-  wire [31:0] div_val = alu_funct[1] ? div_out_rem : div_out_quot;
-  ysyx_23060203_DIV DIV (
-    .clock(clock), .reset(reset), .flush(0),
-    .in_ready(div_in_ready), .in_valid(exec_in_en & div_in_en),
-    .in_sign(div_in_sign), .in_a(in_val_a), .in_b(in_val_b),
-    .out_ready(out_ready), .out_valid(div_out_valid),
-    .out_quot(div_out_quot), .out_rem(div_out_rem)
   );
 
   // -------------------- 跳转 --------------------
@@ -207,12 +172,10 @@ module ysyx_23060203_EXU (
 
   // -------------------- GPR写回 --------------------
   assign out_gpr_waddr = rd;
-  assign out_gpr_wdata = ls[3] ? lsu_out_rdata : (
-    rd_src ? val_a : (
-      mul ? (
-        alu_funct[2] ? div_val : mul_val
-      ) : alu_val
-    )
+  assign out_gpr_wdata = ls[3] ? (
+    lsu_out_rdata
+  ) : (
+    rd_src ? val_a : alu_val
   );
 
   assign exu_rd = rd & {5{valid}};
