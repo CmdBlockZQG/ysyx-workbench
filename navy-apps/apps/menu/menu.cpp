72d2349ca470f8009c7a01405766b77eeedbdb09
tracer-ysyx <tracer@ysyx.org>
2024-07-19 14:22:02 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:22:02 up 8 days,  5:11,  1 user,  load average: 0.46, 0.73, 0.71

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -141,6 +141,7 @@ static void draw_text_row(char *s, int r) {
 
 static void display_menu(int n) {
   clear_display();
+  while (1);
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
