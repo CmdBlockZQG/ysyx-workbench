263e99432f76f533e5ff9abab03a1049ef8d4e4e
tracer-ysyx <tracer@ysyx.org>
2024-07-19 20:40:36 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:40:36 up 8 days, 11:30,  1 user,  load average: 2.05, 2.12, 2.09

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -1,14 +1,15 @@
 #include <am.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 Area heap;
 
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 void putch(char ch) {
-  _syscall_(4, 0, (uintptr_t)&ch, 1);
+  write(1, &ch, 1);
 }
 
 void halt(int code) {
-  _syscall_(0, code, 0, 0);
-  while (1);
+  exit(code);
 }
