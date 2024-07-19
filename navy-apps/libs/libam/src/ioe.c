35c97cf2a5ff958d2a0421c6cc066c3cab861ba4
tracer-ysyx <tracer@ysyx.org>
2024-07-19 19:59:07 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-36-generic #36-Ubuntu SMP PREEMPT_DYNAMIC Mon Jun 10 10:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:59:07 up 8 days, 10:48,  1 user,  load average: 2.73, 2.16, 1.90

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -1,8 +1,26 @@
 #include <am.h>
+#include <assert.h>
+#include <sys/time.h>
+
+void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+  
+}
+
+void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
+  struct timeval tv;
+  assert(gettimeofday(&tv, NULL) == 0);
+  uptime->us = tv.tv_sec * 1000000 + tv.tv_usec;
+}
+
+typedef void (*handler_t)(void *buf);
+static void *lut[128] = {
+  [AM_TIMER_UPTIME] = __am_timer_uptime,
+  [AM_INPUT_KEYBRD] = __am_input_keybrd,
+};
 
 bool ioe_init() {
   return true;
 }
 
-void ioe_read (int reg, void *buf) { }
-void ioe_write(int reg, void *buf) { }
+void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -2,8 +2,13 @@
 
 Area heap;
 
+extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
+
 void putch(char ch) {
+  _syscall_(4, 0, (uintptr_t)&ch, 1);
 }
 
 void halt(int code) {
+  _syscall_(0, code, 0, 0);
+  while (1);
 }
