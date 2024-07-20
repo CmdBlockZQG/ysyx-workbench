bc49ea1bf771445113f24e89d98b52251f7f2ebe
tracer-ysyx <tracer@ysyx.org>
2024-07-20 21:40:04 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:40:04 up 12:40,  1 user,  load average: 0.89, 0.51, 0.48

--- navy-apps/libs/libminiSDL/src/audio.c
+++ navy-apps/libs/libminiSDL/src/audio.c
@@ -1,13 +1,15 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <unistd.h>
 
 #define SBUF_SIZE 64
 
 static void (*callback)(void *userdata, uint8_t *stream, int len);
-static bool pause;
+static bool audio_pause;
 static void *userdata;
 static uint32_t interval;
 static uint32_t last_call;
@@ -15,7 +17,7 @@ static uint8_t *sbuf;
 static bool re_entry;
 
 void CallbackHelper(int force) {
-  if (!callback || pause || re_entry) return;
+  if (!callback || audio_pause || re_entry) return;
   re_entry = true;
   uint32_t t = NDL_GetTicks();
   if (force || t >= last_call + interval) {
@@ -38,7 +40,7 @@ int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained) {
   NDL_OpenAudio(desired->freq, desired->channels, desired->samples);
   callback = desired->callback;
   userdata = desired->userdata;
-  pause = true;
+  audio_pause = true;
   interval = desired->samples * 1000 / desired->freq;
   re_entry = false;
   sbuf = malloc(SBUF_SIZE);
@@ -53,21 +55,68 @@ void SDL_CloseAudio() {
 
 void SDL_PauseAudio(int pause_on) {
   if (pause_on) {
-    pause = true;
+    audio_pause = true;
   } else {
-    pause = false;
+    audio_pause = false;
     CallbackHelper(1);
   }
 }
 
 void SDL_MixAudio(uint8_t *dst, uint8_t *src, uint32_t len, int volume) {
+  int16_t *d = (int16_t *)dst;
+  int16_t *s = (int16_t *)src;
+  for (int i = 0; i < len / 2; ++i) {
+    int t = s[i];
+    d[i] = t * volume / SDL_MIX_MAXVOLUME;
+  }
+}
+
+static void nread(int fd, void *buf, int n) {
+  while (n > 0) {
+    int c = read(fd, buf, n);
+    n -= c;
+    buf += c;
+  }
 }
 
 SDL_AudioSpec *SDL_LoadWAV(const char *file, SDL_AudioSpec *spec, uint8_t **audio_buf, uint32_t *audio_len) {
-  return NULL;
+  printf("%s\n", file);
+  int fd = open(file, O_RDONLY);
+  lseek(fd, 0, SEEK_SET);
+  uint32_t t;
+  uint16_t s;
+  nread(fd, &t, 4);
+  assert(t == 0x46464952);
+
+  lseek(fd, 8, SEEK_SET);
+  nread(fd, &t, 4);
+  assert(t == 0x45564157);
+
+  lseek(fd, 22, SEEK_SET);
+  nread(fd, &s, 2);
+  spec->channels = s - 1;
+
+  lseek(fd, 24, SEEK_SET);
+  nread(fd, &t, 4);
+  spec->freq = t;
+
+  lseek(fd, 40, SEEK_SET);
+  nread(fd, &t, 4);
+  spec->size = t;
+
+  // spec->samples = 1024;
+
+  void *buf = malloc(t);
+  *audio_buf = buf;
+  *audio_len = t;
+  nread(fd, buf, t);
+  close(fd);
+
+  return spec;
 }
 
 void SDL_FreeWAV(uint8_t *audio_buf) {
+  free(audio_buf);
 }
 
 void SDL_LockAudio() {
