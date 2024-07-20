49fe470762561e3cc87423f796ecad4ead132f88
tracer-ysyx <tracer@ysyx.org>
2024-07-20 15:24:05 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:24:05 up  6:24,  1 user,  load average: 0.45, 0.54, 0.70

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -7,12 +7,12 @@
 // #define RVE 1
 #define MAX_CYCLE (uint64_t)(-1) // 10000000
 
-#define ELF 1
+// #define ELF 1
 // #define WATCHPOINT 1
 #define ITRACE 1
 #define PCTRACE 1
 // #define ILOGALL 1
-#define FTRACE 1
+// #define FTRACE 1
 // #define MTRACE 1
 #define DIFFTEST 1
 
