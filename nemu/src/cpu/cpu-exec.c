484c8a75b0aa70eedafa7b56099cb31b417ab7d0
tracer-ysyx <tracer@ysyx.org>
2024-07-20 13:39:14 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:39:14 up  4:39,  1 user,  load average: 0.99, 1.51, 1.63

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,7 +46,6 @@ static void print_iringbuf() {
 void device_update();
 
 #ifdef CONFIG_FTRACE
-static word_t ftrace_dep = 0;
 extern ElfSymbol elf_symbol_list[];
 extern word_t elf_symbol_list_size;
 
@@ -68,31 +67,26 @@ static word_t get_func_sym_ndx(paddr_t p) {
 }
 
 static void ftrace(Decode *s) {
-  static vaddr_t recover_pc = 0;
+  static vaddr_t ret_st[64];
+  static int ftrace_dep = 0;
 
   if (likely(s->dnpc == s->snpc)) return;
   if (elf_symbol_list_size == 0) return; // no elf file
-  if (recover_pc) {
-    if (s->dnpc != recover_pc) return;
-    recover_pc = 0;
-  }
   word_t from = get_func_sym_ndx(s->pc), to = get_func_sym_ndx(s->dnpc);
   if (likely(from == to)) return;
+
   log_write("[FTRACE] " FMT_PADDR ": ", s->pc);
   if (elf_symbol_list[to].addr == s->dnpc) { // call, jump to the begging of a func
     for (int i = 0; i < ftrace_dep; ++i) log_write(" ");
-    ++ftrace_dep;
     log_write("call [%s@" FMT_PADDR "] -> [%s@" FMT_PADDR "]\n",
               elf_symbol_list[from].name,
               elf_symbol_list[from].addr,
               elf_symbol_list[to].name,
               elf_symbol_list[to].addr);
-    if (strstr(elf_symbol_list[to].name, "printf")) {
-      recover_pc = s->snpc;
-    }
+    ret_st[ftrace_dep++] = s->snpc;
   } else { // ret, return to calling position
     Assert(ftrace_dep, "Error occured in FTRACE: negative deepth");
-    --ftrace_dep;
+    while (ret_st[--ftrace_dep] != s->dnpc);
     for (int i = 0; i < ftrace_dep; ++i) log_write(" ");
     log_write("ret [%s@" FMT_PADDR "] -> [%s@" FMT_PADDR "]:" FMT_PADDR "\n",
               elf_symbol_list[from].name,
