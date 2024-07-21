1214610a6c930a8bba5b85267f1f2a1282eef9c7
tracer-ysyx <tracer@ysyx.org>
2024-07-21 12:43:02 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:43:02 up 1 day,  3:43,  1 user,  load average: 0.53, 0.72, 0.71

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -122,7 +122,7 @@ word_t paddr_read(paddr_t addr, int len) {
       log_write(ANSI_NONE "\n");
     }
 #endif
-    assert(addr % len == 0);
+    Assert(addr % len == 0, "Ualigned memory read");
     return pmem_read(addr, len);
   }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
@@ -139,7 +139,7 @@ void paddr_write(paddr_t addr, int len, word_t data) {
       log_write(": " FMT_WORD ANSI_NONE "\n", data);
     }
 #endif
-    assert(addr % len == 0);
+    Assert(addr % len == 0, "Ualigned memory write");
     pmem_write(addr, len, data);
     return; 
   }
