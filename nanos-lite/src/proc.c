cb2839ac5c98160e16d61aea623580a81bc96727
tracer-ysyx <tracer@ysyx.org>
2024-07-22 20:03:43 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:03:43 up 2 days, 11:04,  1 user,  load average: 0.95, 0.81, 0.69

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -35,7 +35,7 @@ void init_proc() {
   // void naive_uload(PCB *pcb, const char *filename);
   // naive_uload(NULL, "/bin/dummy");
 
-  char *const argv[] = { "/bin/pal", "--skip", NULL };
+  char *const argv[] = { "/bin/pal", "--", NULL };
   char *const envp[] = { "KEY=VALUE", NULL };
 
   void context_uload(PCB *pcb, const char *filename, char *const argv[], char *const envp[]);
