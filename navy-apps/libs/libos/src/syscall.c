19e3c599db68ccc05b41af19e692b07e50097074
tracer-ysyx <tracer@ysyx.org>
2024-07-22 23:07:57 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:07:57 up 2 days, 14:08,  1 user,  load average: 0.66, 0.93, 1.00

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -56,7 +56,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   return ret;
 }
 
-static char *const empty[] = { "/bin/nterm", "first", "second", NULL };
+static char *const empty[] = { NULL };
 
 void _exit(int status) {
   // _syscall_(SYS_exit, status, 0, 0);
