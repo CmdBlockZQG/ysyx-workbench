b4a4067c1231a63afefba83faecd757b3d8a662a
tracer-ysyx <tracer@ysyx.org>
2024-07-22 21:47:56 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:47:56 up 2 days, 12:48,  1 user,  load average: 0.75, 0.79, 0.62

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -85,9 +85,9 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   Context *ctx = ucontext(NULL, kstack, (void *)entry);
   void *ustack_top = new_page(8) + 8 * PGSIZE;
 
-  int argc, envc, len = 0;
-  for (argc = 0; argv[argc]; ++argc) len += strlen(argv[argc]) + 1;
-  for (envc = 0; envp[envc]; ++envc) len += strlen(envp[envc]) + 1;
+  int argc = 0, envc = 0, len = 0;
+  for (; argv[argc]; ++argc) len += strlen(argv[argc]) + 1;
+  // for (; envp[envc]; ++envc) len += strlen(envp[envc]) + 1;
   len = ROUNDUP(len, sizeof(uintptr_t));
 
   char *strtab = ustack_top - len;
