6c16c55735d52636acd09a373f438adeb3598616
tracer-ysyx <tracer@ysyx.org>
2024-07-24 14:19:15 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:19:15 up 4 days,  5:19,  1 user,  load average: 0.55, 0.45, 0.42

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,7 +13,7 @@ void switch_boot_pcb() {
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
-    // Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
+    Log("Hello World from Nanos-lite with arg '%p' for the %dth time!", (uintptr_t)arg, j);
     j ++;
     yield();
   }
@@ -30,7 +30,7 @@ void init_proc() {
 
   Log("Initializing processes...");
   
-  // context_kload(&pcb[0], hello_fun, (void *)1);
+  context_kload(&pcb[0], hello_fun, (void *)1);
 
   // void naive_uload(PCB *pcb, const char *filename);
   // naive_uload(NULL, "/bin/dummy");
@@ -38,11 +38,11 @@ void init_proc() {
   char *const argv[] = { "/bin/pal", "--skip", NULL };
   char *const envp[] = { "KEY=VALUE", NULL };
 
-  context_uload(&pcb[0], "/bin/pal", argv, envp);
+  context_uload(&pcb[1], "/bin/pal", argv, envp);
 }
 
 Context* schedule(Context *prev) {
   current->cp = prev;
-  current = &pcb[0];
+  current = current == &pcb[0] ? &pcb[1] : &pcb[0];
   return current->cp;
 }
