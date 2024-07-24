735990e38fdfcdf291198555d00226aedcada77f
tracer-ysyx <tracer@ysyx.org>
2024-07-24 09:38:14 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:38:14 up 4 days, 38 min,  1 user,  load average: 0.64, 0.51, 0.37

--- nemu/src/isa/riscv32/include/isa-def.h
+++ nemu/src/isa/riscv32/include/isa-def.h
@@ -30,6 +30,6 @@ typedef struct {
   } inst;
 } MUXDEF(CONFIG_RV64, riscv64_ISADecodeInfo, riscv32_ISADecodeInfo);
 
-#define isa_mmu_check(vaddr, len, type) (MMU_DIRECT)
+#define isa_mmu_check(vaddr, len, type) ((csr_satp >> 31) ? MMU_TRANSLATE : MMU_DIRECT)
 
 #endif
--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -16,7 +16,29 @@
 #include <isa.h>
 #include <memory/vaddr.h>
 #include <memory/paddr.h>
+#include "../local-include/reg.h"
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  return MEM_RET_FAIL;
+  // TEMP: Sv32 only
+  switch (isa_mmu_check(vaddr, len, type)) {
+    case MMU_FAIL: return MEM_RET_FAIL;
+    case MMU_DIRECT: return vaddr;
+    case MMU_TRANSLATE: ;
+  }
+
+  paddr_t pt1_addr = csr_satp << 12;
+  paddr_t vpn1 = (vaddr >> 22) & 0x3ff;
+  paddr_t pte1_addr = pt1_addr | (vpn1 << 2); 
+  word_t pte1 = paddr_read(pte1_addr, 4);
+  Assert(pte1 & 1, "Invalid page table entry");
+
+  paddr_t pt0_addr = (pte1 << 2) & ~0xfff;
+  paddr_t vpn0 = (vaddr >> 12) & 0x3ff;
+  paddr_t pte0_addr = pt0_addr | (vpn0 << 2);
+  word_t pte0 = paddr_read(pte0_addr, 4);
+  Assert(pte0 & 1, "Invalid page table entry");
+
+  paddr_t paddr = ((pte0 << 2) & ~0xfff) | (vaddr & 0xfff);
+  Assert(paddr == vaddr, "Memory map error");
+  return paddr;
 }
