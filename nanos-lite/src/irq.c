3e6c075491dd58afda4b9c4e235bab8fa3cef072
tracer-ysyx <tracer@ysyx.org>
2024-07-25 11:22:44 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-38-generic #38-Ubuntu SMP PREEMPT_DYNAMIC Fri Jun  7 15:25:01 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:22:44 up  1:01,  1 user,  load average: 0.44, 0.55, 0.51

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -13,6 +13,7 @@ static Context* do_event(Event e, Context* c) {
       do_syscall(c);
     break;
     case EVENT_IRQ_TIMER:
+      Log("timer interrupt received");
       c = schedule(c);
     break;
     default: panic("Unhandled event ID = %d", e.event);
