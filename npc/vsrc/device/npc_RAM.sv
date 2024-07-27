afa9d3b65c1f88cf01c017778aa9ade8295094fa
CmdBlock <54425466+CmdBlockZQG@users.noreply.github.com>
2024-07-27 17:11:12 +0800

    Switch to 32 bit ysyxSoC

--- npc/vsrc/device/npc_RAM.sv
+++ npc/vsrc/device/npc_RAM.sv
@@ -58,7 +58,7 @@ module npc_RAM (
       burst_cnt <= in.arlen;
 
       in.rvalid <= 1;
-      in.rdata <= {2{pmem_read(in.araddr)}};
+      in.rdata <= pmem_read(in.araddr);
       in.rlast <= (in.arlen == 8'b0);
     end
 
@@ -70,7 +70,7 @@ module npc_RAM (
       end else begin
         burst_cnt <= burst_cnt - 1;
         araddr <= araddr_next;
-        in.rdata <= {2{pmem_read(araddr_next)}};
+        in.rdata <= pmem_read(araddr_next);
         in.rlast <= (burst_cnt == 8'h1);
       end
     end
@@ -83,10 +83,10 @@ module npc_RAM (
   wire waddr_valid = waddr_handshake | waddr_valid_reg;
 
   wire wdata_handshake = in.wready & in.wvalid;
-  reg [63:0] wdata_reg;
-  reg [7:0] wmask_reg;
-  wire [63:0] wdata = wdata_handshake ? in.wdata : wdata_reg;
-  wire [7:0] wmask = wdata_handshake ? in.wstrb : wmask_reg;
+  reg [31:0] wdata_reg;
+  reg [3:0] wmask_reg;
+  wire [31:0] wdata = wdata_handshake ? in.wdata : wdata_reg;
+  wire [3:0] wmask = wdata_handshake ? in.wstrb : wmask_reg;
   reg wdata_valid_reg;
   wire wdata_valid = wdata_handshake | wdata_valid_reg;
 
@@ -111,8 +111,7 @@ module npc_RAM (
 
     if (write_en) begin
 `ifndef SYNTHESIS
-      pmem_write({waddr[31:3], 3'b000}, wdata[31:0 ], {4'b0, wmask[3:0]});
-      pmem_write({waddr[31:3], 3'b100}, wdata[63:32], {4'b0, wmask[7:4]});
+      pmem_write({waddr[31:2], 2'b00}, wdata[31:0], {4'b0, wmask[3:0]});
 `endif
 
       in.bresp <= 2'b00;
