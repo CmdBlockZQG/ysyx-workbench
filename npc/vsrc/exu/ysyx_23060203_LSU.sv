495cf6b0f2b5967617e587c2b6fc916610091196
tracer-ysyx <tracer@ysyx.org>
2024-07-29 21:47:11 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:47:11 up 2 days,  8:15,  1 user,  load average: 0.41, 0.46, 0.41

--- npc/vsrc/exu/ysyx_23060203_LSU.sv
+++ npc/vsrc/exu/ysyx_23060203_LSU.sv
@@ -190,7 +190,7 @@ module ysyx_23060203_LSU (
       event_mem_read(addr, {29'b0, mem_r.arsize}, alu_val);
     end
     if (mem_w.awready & mem_w.awvalid) begin
-      event_mem_write(addr, {29'b0, mem_w.awsize}, val_c);
+      event_mem_write(addr, {29'b0, mem_w.awsize}, alu_val);
     end
   end
 `endif
