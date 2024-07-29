a5a4cb0dc074245e42eeeffbc47a59596d3d1f85
tracer-ysyx <tracer@ysyx.org>
2024-07-29 22:16:07 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:16:07 up 2 days,  8:44,  1 user,  load average: 0.77, 0.66, 0.51

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -9,11 +9,11 @@
 
 // #define ELF 1
 // #define WATCHPOINT 1
-#define ITRACE 1
+// #define ITRACE 1
 #define PCTRACE 1
-#define ILOGALL 1
+// #define ILOGALL 1
 // #define FTRACE 1
-#define MTRACE 1
+// #define MTRACE 1
 #define DIFFTEST 1
 
 #endif
