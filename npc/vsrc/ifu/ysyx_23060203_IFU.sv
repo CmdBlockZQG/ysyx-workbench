ac3f3042d5917f577c9d83546de17befac3f0fac
tracer-ysyx <tracer@ysyx.org>
2024-08-01 16:26:09 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:26:09 up 5 days,  2:54,  1 user,  load average: 1.38, 0.79, 0.82

--- npc/vsrc/ifu/ysyx_23060203_IFU.sv
+++ npc/vsrc/ifu/ysyx_23060203_IFU.sv
@@ -47,15 +47,30 @@ module ysyx_23060203_IFU (
   wire [31:0] dnpc = cs_flush ? cs_dnpc : jump_dnpc;
   // reg
   reg flush_r;
+  wire flush_w = flush | flush_r;
   wire flush_r_next = flush_w & ~hit;
   reg [31:0] dnpc_r;
   wire [31:0] dnpc_r_next = (flush & ~hit) ? dnpc : dnpc_r;
 
+  // -------------------- output --------------------
+  reg out_valid_r, out_valid_r_next;
+  wire out_step_en = ~out_valid_r | out_ready;
+  wire [31:0] out_pc_next   = (~flush_w & hit & out_step_en) ? fetch_pc   : out_pc;
+  wire [31:0] out_inst_next = (~flush_w & hit & out_step_en) ? cache_inst : out_inst;
+  always_comb begin
+    out_valid_r_next = out_valid_r;
+    if (flush) begin
+      out_valid_r_next = 0;
+    end else if (out_valid_r) begin
+      if (out_ready) begin
+        out_valid_r_next = hit;
+      end
+    end else begin
+      if (hit) out_valid_r_next = 1;
+    end
+  end
 
   // -------------------- ? --------------------
-  reg out_valid_r;
-  wire flush_w = flush | flush_r;
-
   always @(posedge clock) begin
     if (reset) begin
       out_valid_r <= 0;
@@ -72,22 +87,15 @@ module ysyx_23060203_IFU (
       out_pc <= out_pc_next;
       out_inst <= out_inst_next;
       fetch_pc <= fetch_pc_next;
-      flush_r <= flush_w & ~hit;
+      flush_r <= flush_r_next;
       dnpc_r <= dnpc_r_next;
     end
   end
 
-  wire out_step_en = ~out_valid_r | out_ready;
-
-  wire [31:0] out_pc_next   = (~flush_w & hit & out_step_en) ? fetch_pc   : out_pc;
-  wire [31:0] out_inst_next = (~flush_w & hit & out_step_en) ? cache_inst : out_inst;
-
-
   reg [31:0] fetch_pc_next;
-  reg out_valid_r_next;
 
   always_comb begin
-    out_valid_r_next = out_valid_r;
+    // out_valid_r_next = out_valid_r;
     fetch_pc_next = fetch_pc;
 
     if (flush_r) begin
@@ -97,21 +105,21 @@ module ysyx_23060203_IFU (
     end else if (out_valid_r) begin
       if (out_ready) begin
         if (hit) begin
-          out_valid_r_next = 1;
+          // out_valid_r_next = 1;
           fetch_pc_next = fetch_pc_pred;
         end else begin
-          out_valid_r_next = 0;
+          // out_valid_r_next = 0;
         end
       end
     end else begin
       if (hit) begin
-        out_valid_r_next = 1;
+        // out_valid_r_next = 1;
         fetch_pc_next = fetch_pc_pred;
       end
     end
 
     if (flush) begin
-      out_valid_r_next = 0;
+      // out_valid_r_next = 0;
       if (hit) begin
         fetch_pc_next = dnpc;
       end
