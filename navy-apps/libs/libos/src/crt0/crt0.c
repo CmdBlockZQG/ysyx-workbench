8f45aef8ada7b9c8499f51aff972d9fafcf6e131
tracer-ysyx <tracer@ysyx.org>
2024-08-05 22:16:14 +0800

    > compile NEMU ysyx_23060203 朱庆刚 Linux CmdBlock-Dev-Laptop 6.8.0-39-generic #39-Ubuntu SMP PREEMPT_DYNAMIC Fri Jul  5 21:49:14 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:16:14 up 9 days,  8:45,  1 user,  load average: 0.53, 0.42, 0.70

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[], char *envp[]);
 void __libc_init_array();
@@ -12,6 +13,9 @@ void call_main(uintptr_t *args) {
   char **envp = (char **)args + 1 + argc + 1;
 
   __libc_init_array();
+
+  printf("start!!!!!\n");
+
   exit(main(argc, argv, envp));
   assert(0);
 }
