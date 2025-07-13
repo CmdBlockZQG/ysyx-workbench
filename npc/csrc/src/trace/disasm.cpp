9d7d842fde154ea2415f2d04341198e823c980b7
tracer-ysyx <tracer@ysyx.org>
2025-07-13 23:17:55 +0800

    > sim RTL ysyx_23060203 朱庆刚 Linux cmdblockarch 6.15.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Jul 2025 17:10:18 +0000 x86_64 GNU/Linux  23:17:55 up 42 min,  1 user,  load average: 1.29, 1.31, 1.21

--- npc/csrc/src/trace/disasm.cpp
+++ npc/csrc/src/trace/disasm.cpp
@@ -1,5 +1,7 @@
 #ifdef ITRACE
 
+#include <debug.h>
+
 #if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
@@ -77,6 +79,7 @@ void init_disasm(const char *triple) {
   gIP->setPrintBranchImmAsAddress(true);
   if (isa == "riscv32" || isa == "riscv64")
     gIP->applyTargetSpecificCLOption("no-aliases");
+  Log("disasm init");
 }
 
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
